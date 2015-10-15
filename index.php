6d273e5bd8aa7d72635b26a3c56563586fe6890c
Melanie Bakovic <mb437@cs.duke.edu>
2015-10-14 23:04:36 -0400

    fixed

--- index.php
+++ index.php
@@ -1,4 +1,41 @@
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title></title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
 <?php
-	$current_page = "index";
-	require_once("header.php");	
+$current_page = "index";
+require_once("header.php");
 ?>
+
+<a href="#">
+<div id="allclothes">
+     <p class="allclothestext">
+      All Clothes
+      </p>
+</div>
+
+</a>
+<a href="#">
+
+<div id="costumes">
+     <p class="allclothestext">
+     Costumes
+      </p>
+</div>
+</a>
+
+<a href="#">
+
+<div id="formalclothes">
+     <p class="allclothestext">
+      Formal Clothes
+     </p>
+</div>
+
+</a>
+
+
+</body>
