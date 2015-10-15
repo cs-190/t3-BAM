2c59e25c5d2316bfaf00a27bf72e918a3145ccae
Melanie Bakovic <mb437@cs.duke.edu>
2015-10-14 23:11:37 -0400

    header

--- header.php
+++ header.php
@@ -5,8 +5,15 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<div id=header>
+<form id="searchbar" method="get" action="">
+	<input type="text" class="searchbar" size="21" maxlength="120">
+	<input type="submit" value="search" class="searchbutton">
+</form>
 <nav class="navbar">
 	<a class="<?php if($current_page == "index") {echo "active";} ?>" href="index.php">Home</a>
 	<a class="<?php if($current_page == "about") {echo "active";} ?>" href="about.php">About</a>
 	<a class="<?php if($current_page == "contact") {echo "active";} ?>" href="contact.php">Contact</a>
 </nav>
+</div>
+</body>
