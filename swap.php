130f97fa6163aa49bf2503374b69426cc91ee988
Anna Sun <ays7@cs.duke.edu>
2015-11-30 17:46:34 -0500

    user interface updates

--- swap.php
+++ swap.php
@@ -7,25 +7,15 @@
 	$current_page = "swap";
 	require_once("header.php");
 
-	function output($name,$description){
+	function output($name,$description,$price){
 		return '<div class="col-sm-4 col-lg-4 col-md-4">
 			<div class="thumbnail">
 				<img src="https://placehold.it/320x150" alt="">
 				<div class="caption">
-					<h4 class="pull-right">$64.99</h4>
+					<h4 class="pull-right">$'."$price".'</h4>
 					<h4><a href="#">'."$name".'</a>
 					</h4>
 					<p>'."$description".'</p>
-				</div>
-				<div class="ratings">
-					<p class="pull-right">12 reviews</p>
-					<p>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star"></span>
-						<span class="glyphicon glyphicon-star-empty"></span>
-					</p>
 				</div>
 			</div>
 		</div>';
@@ -37,14 +27,14 @@
 		$conn = new PDO($dsn, $username, $password);
 		// set the PDO error mode to exception
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql = "SELECT name,description FROM Items";
+		$sql = "SELECT name,description,price FROM Items";
 		$ready = $conn->prepare($sql);
 		$ready->execute();
 		$allitems = array();
 		$counter = 0;
 		$items = $ready->fetchAll();
 		foreach($items as $row){
-			$allitems[$counter] = output($row["name"],$row["description"]);
+			$allitems[$counter] = output($row["name"],$row["description"],$row["price"]);
 			$counter = $counter +1;
 		}
 		return $allitems;
@@ -72,38 +62,7 @@
             </div>
 
             <div class="col-md-9">
-
-                <div class="row carousel-holder">
-
-                    <div class="col-md-12">
-                        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                            <ol class="carousel-indicators">
-                                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                            </ol>
-                            <div class="carousel-inner">
-                                <div class="item active">
-                                    <img class="slide-image" src="https://placehold.it/800x300" alt="">
-                                </div>
-                                <div class="item">
-                                    <img class="slide-image" src="https://placehold.it/800x300" alt="">
-                                </div>
-                                <div class="item">
-                                    <img class="slide-image" src="https://placehold.it/800x300" alt="">
-                                </div>
-                            </div>
-                            <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                                <span class="glyphicon glyphicon-chevron-left"></span>
-                            </a>
-                            <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                                <span class="glyphicon glyphicon-chevron-right"></span>
-                            </a>
-                        </div>
-                    </div>
-
-                </div>
-
+				<br>
                 <div class="row">
 					<?php
 						$finalitems = getItems();
