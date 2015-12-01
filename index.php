f795efddedd476d0c2ca34860bc57920987f98fb
Melanie Bakovic <mb437@cs.duke.edu>
2015-11-30 19:18:45 -0500

    merge

--- index.php
+++ index.php
@@ -49,48 +49,5 @@ require_once("header.php");
 
 </div>
 </div>
-<div class="row"></div>
-<div class="imagecontain">
-	<a href="swap.php">
-     <p class="allclothestext"> shop all clothes </p>
-	</a>
-</div>
-
-<div class ="decadecontain">
-	<a href="">
-	<p class ="categorytext">decades</p>
-	</a>
-</div>
-
-<div class ="countrycontain">
-	<a href="">
-	<p class ="categorytext">country</p>
-	</a>
-</div>
-
-<div class ="animalcontain">
-	<a href="">
-	<p class ="categorytext">animal</p>
-	</a>
-</div>
-
-<div class ="neoncontain">
-	<a href="">
-	<p class ="categorytext">neon</p>
-	</a>
-</div>
-
-<div class ="sportscontain">
-	<a href="">
-	<p class ="categorytext">sports</p>
-	</a>
-</div>
-
-<div class ="tropicalcontain">
-	<a href="">
-	<p class ="categorytext">tropical</p>
-	</a>
-</div>
-
 
 </body>
